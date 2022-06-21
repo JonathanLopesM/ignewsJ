498f914cf93aabe74202e7e1ed4f860def217738
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2022-06-21 19:41:25 -0300

    fallback

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -58,7 +58,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 export const getStaticPaths : GetStaticPaths = async () => {
   return { //geração estatica definida
     paths : [],
-    fallback : false//true //false //'blocking'
+    fallback : 'blocking'//true //false //'blocking'
   }
 }
 
